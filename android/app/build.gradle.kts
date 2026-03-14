c4acbfdd05fabaa5041105132911f1bc8969f803
uveshsiddiqui90 <uveshsiddiqui90@gmail.com>
2026-03-14 23:16:07 +0530

    almost completed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,15 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -7,9 +19,18 @@ plugins {
 }
 
 android {
-    namespace = "com.example.watertime"
+    namespace = "com.uveshapps.watertime"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true 
@@ -23,7 +44,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.watertime"
+        applicationId = "com.uveshapps.watertime"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
@@ -35,11 +56,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            
            
             proguardFiles(
             getDefaultProguardFile("proguard-android-optimize.txt"),
--- android/app/src/main/kotlin/com/uveshapps/watertime/MainActivity.kt
+++ android/app/src/main/kotlin/com/uveshapps/watertime/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.watertime
+package com.uveshapps.watertime
 
 import io.flutter.embedding.android.FlutterActivity
 
